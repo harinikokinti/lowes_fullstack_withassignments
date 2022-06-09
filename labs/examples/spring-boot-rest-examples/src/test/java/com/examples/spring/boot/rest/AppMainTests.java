29b101cc65db4115ab5dfa3f05eddcf5353a4583
asaravanakumar <saravana.anbarasu@ramselabs.com>
2022-06-09 11:40:16 +0530

    spring boot mvc and rest examples

---
+++ labs/examples/spring-boot-rest-examples/src/test/java/com/examples/spring/boot/rest/AppMainTests.java
@@ -0,0 +1,12 @@
+package com.examples.spring.boot.rest;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class AppMainTests {
+
+	@org.junit.jupiter.api.Test
+	public void contextLoads() {
+	}
+
+}
