7ab7a197840f517c45534affc74770721100cf3e
asaravanakumar <saravana.anbarasu@ramselabs.com>
2022-06-09 12:43:32 +0530

    spring boot rest examples refactored

--- labs/examples/spring-boot-rest-examples/src/test/java/com/examples/spring/boot/rest/AppMainTests.java
+++ labs/examples/spring-boot-rest-examples/src/test/java/com/examples/spring/boot/rest/AppMainTests.java
@@ -1,11 +1,12 @@
 package com.examples.spring.boot.rest;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class AppMainTests {
 
-	@org.junit.jupiter.api.Test
+	@Test
 	public void contextLoads() {
 	}
 
